6cc20e0b164473f5be4818865980f1985fb8767b
Hajime Todo <samsepylot@gmail.com>
2022-07-20 10:58:10 +0900

    GA設置

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics strategy="lazyOnload" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 function isDarkMode() {
