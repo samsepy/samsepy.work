f61cd2bf2a449abc601280fdbd1552c06151b2bf
Hajime Todo <samsepylot@gmail.com>
2022-07-24 01:40:19 +0900

    footerの余白調整等

--- components/layout.tsx
+++ components/layout.tsx
@@ -54,20 +54,25 @@ export default function Layout({
 
   return (
     <div className={currentMode()}>
-      <Head>
-        <title>{title}</title>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta name="description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={title} />
-        <meta property="og:site_name" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={imgUrl} />
-        <link rel="canonical" href={url} />
-      </Head>
-      <main>{children}</main>
-      <Footer />
+      <div className={styles.container}>
+        <Head>
+          <title>{title}</title>
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0"
+          />
+          <meta name="description" content={description} />
+          <meta property="og:url" content={url} />
+          <meta property="og:title" content={title} />
+          <meta property="og:site_name" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={imgUrl} />
+          <link rel="canonical" href={url} />
+        </Head>
+        <main>{children}</main>
+        <Footer />
+      </div>
       <span
         className={`${currentSwitch()} ${styles.floatingButton} ${
           styles.modeSwitchButton
