5159dd5b921076c54427cc660c77b2b42ff0fef4
Hajime Todo <samsepylot@gmail.com>
2022-07-26 16:19:50 +0900

    暗黙のany型を禁止にしてたらbuild時にこけてたので修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics strategy="lazyOnload" />
